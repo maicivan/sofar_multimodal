8081a79745d3c2785b43c843d4356545ada09bc0
Zenatek <filippo.lapide@gmail.com>
2019-10-07 15:42:31 +0200

    change logic

--- scripts/featureScheduler.py
+++ scripts/featureScheduler.py
@@ -5,24 +5,31 @@ from std_msgs.msg import *
 from sofar_multimodal.msg import *
 
 #ASSEGNAMENTO 
-union_objs = unionMsg()
+adaptObj = adapter()
+commonObj = commonFeature()
+
+union_objs = selectorMatcher()
+
 intersect_obj = selectorMatcher()
+
 started = False
 
 ###PUBLISHER
 pub_intersection = rospy.Publisher('/featureScheduler/pubIntersection', selectorMatcher, queue_size=10)
-pub_union = rospy.Publisher('/featureScheduler/pubUnion', unionMsg, queue_size=10)
+pub_union = rospy.Publisher('/featureScheduler/pubUnion', selectorMatcher, queue_size=10)
 
 ###CALLBACKS
-def callbackPitt(TrackedShapes):
-    global started, union_objs
-    union_objs.union_objs.append(TrackedShapes)
+def callbackPitt(adapter):
+    global started, union_objs, commonObj
+    commonObj.common.append(adapter)
+    union_objs.matcher.append(commonObj)
     if(not started):
         started = True
 
-def callbackTensor(TensorOutput):
-    global started, union_objs
-    union_objs.union_objs.append(TensorOutput)
+def callbackTensor(adapter):
+    global started, union_objs, commonObj
+    commonObj.common.append(adapter)
+    union_objs.matcher.append(commonObj)
     if(not started):
         started = True
 
@@ -31,14 +38,14 @@ def timer_callback(event):
     global started, pub_union, union_objs
     if (started):
         pub_union.publish(union_objs)
-        union_objs.union_objs[:] = []
+        union_objs.matcher[:] = []
     
 ###LISTENER
 
 def listener():
     rospy.init_node('listener', anonymous=True)
-    rospy.Subscriber('PittChannel', TrackedShapes, callbackPitt)
-    rospy.Subscriber('TensorChannel', TensorOutput, callbackTensor)
+    rospy.Subscriber('outputAdapterPitt', adapter, callbackPitt)
+    rospy.Subscriber('outputAdapterTensor', adapter, callbackTensor)
 
     timer = rospy.Timer(rospy.Duration(0.5), timer_callback)
 
