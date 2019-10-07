56543588399263c833f2df0b219ac93c80f9d633
Zenatek <filippo.lapide@gmail.com>
2019-10-07 17:44:06 +0200

    scheletro intersezione

--- scripts/featureScheduler.py
+++ scripts/featureScheduler.py
@@ -7,16 +7,26 @@ from sofar_multimodal.msg import *
 #ASSEGNAMENTO
 commonObj = commonFeature()
 union_objs = selectorMatcher()
-intersect_obj = selectorMatcher()
+#intersect_obj = selectorMatcher()
+
+def compare(adapter, adapter):
+	if(commonObj.common.id_mod )
 
 ###CALLBACKS
 def callbackPitt(adapter):
+	##UNION
     commonObj.common.append(adapter)
     union_objs.matcher.append(commonObj)
+	##INTERSECT
+	#intersect_obj.matcher.append(commonObj)
 
 def callbackTensor(adapter):
+	#UNION
     commonObj.common.append(adapter)
     union_objs.matcher.append(commonObj)
+	#INTERSECT
+	#intersect_obj.matcher.append(commonObj)
+
 
 if __name__ == '__main__':
 	rospy.loginfo("In esecuzione...")
@@ -25,11 +35,17 @@ if __name__ == '__main__':
 	rospy.Subscriber('outputAdapterPitt', adapter, callbackPitt)
 	rospy.Subscriber('outputAdapterTensor', adapter, callbackTensor)
 	###PUBLISHERS
-	pub_intersection = rospy.Publisher('/featureScheduler/pubIntersection', selectorMatcher, queue_size=10)
+	pub_intersect = rospy.Publisher('/featureScheduler/pubIntersection', selectorMatcher, queue_size=10)
 	pub_union = rospy.Publisher('/featureScheduler/pubUnion', selectorMatcher, queue_size=10)
 	rate = rospy.Rate(10)
 	while not rospy.is_shutdown():
+		###UNION
 		pub_union.publish(union_objs)
 		commonObj.common[:] = []
 		union_objs.matcher[:] = []
-		rate.sleep()
+		###INTERSECT -- DA FINIRE SOLO IDEA
+		# compare(adapter, adapter)
+		# pub_intersect.publish(intersect_obj)
+		# intersect_obj.matcher[:] = []
+
+		# rate.sleep()
